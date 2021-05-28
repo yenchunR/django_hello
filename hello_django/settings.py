7cb41c2937ac29b3f463c6c60ccc2891ce252ea7
Ruby Ou <rubyruru3205@gmail.com>
2021-05-28 11:20:37 +0800

    myhellozz#1

--- hello_django/settings.py
+++ hello_django/settings.py
@@ -25,8 +25,10 @@ SECRET_KEY = 'django-insecure-ricghgu2nlzpg9^$nl+0cbiw)9*nluq+_y8z959xx(1+d6hc!i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['myhellozz.herokuapp.com/']
 
+#新增static資料夾的路徑設定
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Application definition
 
@@ -42,6 +44,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    ##
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
